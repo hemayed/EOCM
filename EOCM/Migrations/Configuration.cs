c166cd2590b86b63681fc6a9419724d333feb1d2
hemayed <hemayed@ieee.org>
2015-05-04 10:43:19 +0200

    remove and add folders and images

---
+++ EOCM/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace EOCM.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EOCM.Models.EOCMDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EOCM.Models.EOCMDB";
+        }
+
+        protected override void Seed(EOCM.Models.EOCMDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
