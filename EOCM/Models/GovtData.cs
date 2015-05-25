c16ca4c5a10274a9f6fab6d740d3bb147db05eec
hemayed <hemayed@ieee.org>
2015-05-25 18:07:52 +0200

    added images and new models

---
+++ EOCM/Models/GovtData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EOCM.Models
+{
+    public class GovtData
+    {
+        
+            public string Govt_ID { get; set; }
+            public string Govt_Name { get; set; }
+            public decimal Govt_Lat { get; set; }
+            public decimal Govt_Long { get; set; }
+            public int Cluster_Num { get; set; }
+            public  int[] Sector_Num {get; set;}
+    }
+}
